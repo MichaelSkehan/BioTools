d83aa2e1f14ea625f5896d369f8ab436ef4a2083
Michael <160975477+MichaelSkehan@users.noreply.github.com>
2025-08-27 14:15:46 +0100

    created a class for future functionality, added a function to determine annealing time based on tm and length

--- main.py
+++ main.py
@@ -1,35 +1,64 @@
-def calculate_tm(sequence: str) -> float:
+
+class Primer:
     """
-    Calculate Tm (℃) of a DNA sequence.
-    Formula: Tm = 2*(NA + NT) + 4*(NC + NG) - 5
+    A class representing a DNA primer.
 
-    Parameters:
-        sequence (str): DNA sequence (string of A, T, C, G)
-
-    Returns:
-        float: Calculated Tm in Celsius
+    Attributes:
+        sequence (str): The DNA sequence (A,T,C,G)
+        length (int): The length of the DNA sequence
+        tm (float): Calculated melting temperature in Celsius
+        annealing_time (int): Recommended annealing time in seconds
     """
-    # Convert to uppercase to avoid case issues
-    seq = sequence.upper()
+    def __init__(self, sequence:str):
+        """
+        Initialise a Primer object
 
-    # Count nucleotides
-    NA = seq.count("A")
-    NT = seq.count("T")
-    NC = seq.count("C")
-    NG = seq.count("G")
+        Parameters:
+            sequence (str) : DNA sequence (A,T,G,C)
+        """
+        self.sequence = sequence.upper()
+        self.length = len(self.sequence)
+        self.tm = self.calculate_tm()
+        self.annealing_time = self.determine_annealing_time()
 
-    # Apply formula
-    tm = (2 * (NA + NT)) + (4 * (NC + NG)) - 5
+    def calculate_tm(self) -> float:
+        """
+        Calculate Tm (℃) of a DNA sequence.
 
-    return tm
+        Returns:
+            float: Calculated Tm in Celsius
+        """
 
+        # Count nucleotides
+        NA = self.sequence.count("A")
+        NT = self.sequence.count("T")
+        NC = self.sequence.count("C")
+        NG = self.sequence.count("G")
+
+        # Apply formula
+        tm = (2 * (NA + NT)) + (4 * (NC + NG)) - 5
+
+        return tm
+
+    def determine_annealing_time(self) -> int:
+        """
+        Determine annealing time based on length and Tm
+
+        Returns:
+            int: Annealing time
+        """
+        if self.length < 25:
+            return 5
+        elif self.tm >= 55:
+            return 5
+        else:
+            return 15
 
 if __name__ == "__main__":
     # Ask user for input
     user_seq = input("Enter a DNA sequence (A, T, C, G): ")
+    primer = Primer(user_seq)
 
-    # Calculate Tm
-    tm_value = calculate_tm(user_seq)
+    tm_value = Primer.determine_annealing_time(primer)
 
-    print(f"Tm of {user_seq.upper()} = {tm_value} ℃")
-    print("Length: ", len(user_seq))
+    print(f"Tm value: {tm_value}")
