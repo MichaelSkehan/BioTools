cbd69cff9216048dc8672c51bff85c6a66bf81f3
Michael <160975477+MichaelSkehan@users.noreply.github.com>
2025-09-02 19:22:23 +0100

    added docstring to ligate function

--- pASPIre_adaptor_ligation/adaptor_ligation.py
+++ pASPIre_adaptor_ligation/adaptor_ligation.py
@@ -1,6 +1,20 @@
 import adaptors
 
 def ligate(left, seq, right):
+    """
+    Ligates a left adaptor, sequence and right adaptor.
+
+
+    :param left: The left adaptor to ligate.
+    :type: str
+    :param seq: The sequence to ligate.
+    :type: str
+    :param right: The right adaptor to ligate.
+    :type: str
+
+    :return: The ligated sequence.
+    :rtype: str
+    """
     return left + seq + right
 
 if __name__ == '__main__':
