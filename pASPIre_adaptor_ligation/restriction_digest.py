9c1829b33d5a22dc8bdca44d09924509ea6b86e5
Michael <160975477+MichaelSkehan@users.noreply.github.com>
2025-09-02 22:03:31 +0100

    modified function to take a csv containing sequence and adaptor information as input

--- pASPIre_adaptor_ligation/restriction_digest.py
+++ pASPIre_adaptor_ligation/restriction_digest.py
@@ -1,5 +1,6 @@
 from Bio.Restriction import *
 from Bio.Seq import Seq
+from parse_csv import parse_csv
 
 def extract_target_seq(seq):
     """
@@ -7,36 +8,46 @@ def extract_target_seq(seq):
     :param seq: Bio.Seq object.
     :return:
     """
+    digest_products = []
+    parsed_seq = parse_csv(seq)
     rb = NcoI + SacI
-    analysis = Analysis(rb, test_seq, linear=False)
-    cut_map = analysis.full()
+    for plasmid in parsed_seq:
+        fragment_list = []
+        plasmid_seq = Seq(plasmid[0])
 
-    cut_sites = []
+        analysis = Analysis(rb, plasmid_seq, linear=False)
+        cut_map = analysis.full()
 
-    for re, loc in cut_map.items():
-        cut_sites.extend(loc)
+        cut_sites = []
 
-    cut_pos = (sorted(set(cut_sites)))
+        for re, loc in cut_map.items():
+            cut_sites.extend(loc)
 
-    fragments = []
-    for i in range(len(cut_pos) - 1):
-        start = cut_pos[i]
-        end = cut_pos[(i+1)]
+        cut_pos = (sorted(set(cut_sites)))
 
-        fragments.append(seq[start-1:end-1])
+        fragments = []
+        for i in range(len(cut_pos) - 1):
+            start = cut_pos[i]
+            end = cut_pos[(i+1)]
 
-    for frag in fragments:
-        if len(frag) == 144:
-            fragments.remove(frag)
+            fragments.append(plasmid_seq[start-1:end-1])
 
-    if len(fragments) > 1:
-        return "Multiple digest products present, check sequence"
-    else:
-        return fragments[0]
+        for frag in fragments:
+            if len(frag) == 144:
+                fragments.remove(frag)
+
+        if len(fragments) > 1:
+            return "Multiple digest products present, check sequence"
+        else:
+            fragment_list.append(fragments[0])
+            fragment_list.append(plasmid[1])
+            fragment_list.append(plasmid[2])
+        digest_products.append(fragment_list)
+    return digest_products
+
 
 if __name__ == '__main__':
-    test_seq = Seq(
-        "CCCGCCGCATCCATACCGCCAGTTGTTTACCCTCACAACGTTCCAGTAACCGGGCATGTTCATCATCAGTAACCCGTATCGTGAGCATCCTCTCTCGTTTCATCGGTATCATTACCCCCATGAACAGAAATCCCCCTTACACGGAGGCATCAGTGACCAAACAGGAAAAAACCGCCCTTAACATGGCCCGCTTTATCAGAAGCCAGACATTAACGCTTCTGGAGAAACTCAACGAGCTGGACGCGGATGAACAGGCAGACATCTGTGAAAGCCTTCACGACCACGCTGATGAGCTTTACCGCAGCTGCCTCGCGCGTTTCGGTGATGACGGTGAAAACCTCTGACACATGCAGCTCCCGGAGACGGTCACAGCTTGTCTGTAAGCGGATGCCGGGAGCAGACAAGCCCGTCAGGGCGCGTCAGCGGGTGTTGGCGGGTGTCGGGGCGCAGCCATGACCCAGTCACGTAGCGATAGCGGAGTGTATACTGGCTTAACTATGCGGCATCAGAGCAGATTGTACTGAGAGTGCACCATTGCGGTGTGAAATACCGCACAGATGCGTAAGGAGAAAATACCGCATCAGGCGCTCTTCCGCTTCCTCGCTCACTGACTCGCTGCGCTCGGTCGTTCGGCTGCGGCGAGCGGTATCAGCTCACTCAAAGGCGGTAATACGGTTATCCACAGAATCAGGGGATAACGCAGGAAAGAACATGTGAGCAAAAGGCCAGCAAAAGGCCAGGAACCGTAAAAAGGCCGCGTTGCTGGCGTTTTTCCATAGGCTCCGCCCCCCTGACGAGCATCACAAAAATCGACGCTCAAGTCAGAGGTGGCGAAACCCGACAGGACTATAAAGATACCAGGCGTTTCCCCCTGGAAGCTCCCTCGTGCGCTCTCCTGTTCCGACCCTGCCGCTTACCGGATACCTGTCCGCCTTTCTCCCTTCGGGAAGCGTGGCGCTTTCTCATAGCTCACGCTGTAGGTATCTCAGTTCGGTGTAGGTCGTTCGCTCCAAGCTGGGCTGTGTGCACGAACCCCCCGTTCAGCCCGACCGCTGCGCCTTATCCGGTAACTATCGTCTTGAGTCCAACCCGGTAAGACACGACTTATCGCCACTGGCAGCAGCCACTGGTAACAGGATTAGCAGAGCGAGGTATGTAGGCGGTGCTACAGAGTTCTTGAAGTGGTGGCCTAACTACGGCTACACTAGAAGGACAGTATTTGGTATCTGCGCTCTGCTGAAGCCAGTTACCTTCGGAAAAAGAGTTGGTAGCTCTTGATCCGGCAAACAAACCACCGCTGGTAGCGGTGGTTTTTTTGTTTGCAAGCAGCAGATTACGCGCAGAAAAAAAGGATCTCAAGAAGATCCTTTGATCTTTTCTACGGGGccggcctacggccagcctcgcagagcaggattcccgttgagcaccgccaggtgcgaataagggacagtgaagaaggaacacccgctcgcgggtgggcctacttcacctatcctgcccggctgacgccgttggatacaccaaggaaagtctacacgaaccctttggcaaaatcctgtatatcgtgcgaaaaaggatggatataccgaaaaaatcgctataatgaccccgaagcagggttatgcagcggaaaaggacaacgcgcggaccgcggtccaattaattattagaaaaattcatccagcatcagatgaaattgcagtttgttcatatccggattatcaatgccatatttctgaaacagacgtttttgcaggctcgggctaaattcgcccaggcagttccacagaatggccagatcctgataacgatccgcaatgcccacacggcccacatcaatgcagccaatcagtttgccttcatcgaaaatcaggttatccaggctaaaatcgccgtgggtcaccacgctatccgggctaaacggcagcagtttatgcatttctttccacacctgttccaccggccagccgttacgttcatcatcaaaatcgctcgcatccaccaggccgttgttcatacggctctgcgcctgggccagacgaaacacacgatcgctgttaaacgggcagttgcacaccggaatgctatgcagacgacgcagaaacacggccagcgcatccacaatgttttcgccgctatccggatattcttccagcacctgaaacgcggttttgcccggaatcgcggtggtcagcagccacgcatcatccggggtgcgaataaaatgtttaatggtcggcagcggcataaattcggtcagccagttcagacgcaccatttcatcggtcacatcgttcgccacgctgcctttgccatgtttcagaaacagttccggcgcatccggtttgccatacagacgataaatggtcgcgccgctctgacccacgttatcacgcgcccatttatagccatacagatccgcatccatgttgctgttcagacgcggacggctacagctcgtttcacgctgaatatggctcataacaccccttgtattactgtttatgtaagcagacagttttattgttcatgatgatatatttttatcttgtgcaatgtaacatcagagattttgagacacaaatttaaatcgtaattattggggacccctggattctcaccaataaaaaacgcccggcggcaaccgagcgttctgaacaaatccagatggagttctgaggtcattactggatctatcaacaggagtccaagaagcttcacatctgcagtaatcggccggcttgtcgacgacggcggtctccgtcgtcaggatcatccgggcccatggatcgcttagtcacctttgggccacggtccgctaccttacaggaatagtactcgtcctttaatttcccttgcatatatgttgcgtttcttcgacctagtactcgctcccttaggagaaagacagatagcttcttacccgccatgggggtttgtaccgtacaccactgagaccgcggtggttgaccagacaaaccacgaaggttctgttaagtaactgaacccaatgtcgttagtgacgcttacctcttaagaggtcactgacctaacaggatcccaccacaattcagcaaattgtgaacatcatcacgttcatctttccctggttgccaatggcccattttcctgtcagtaacgagaaggtcgcgaattcaggcgctttttagactggtcgtagcaacaaaatcaggcacaggcagaacaacaatgatcaaggcgacggacagaaaactgGGTGGAGGCGGTTCTgaaaatttatacttccaatccGGAGGTGGAGGCTCTCGAGCTctggttgttattcgtctgagccgtgttaccgatgcaaccaccagtccggaacgtcagctggaaagctgtcagcagctgtgtgcacagcgtggttgggatgttgttggtgttgcagaggatctggatgttagcggtgcagttgatccgtttgatcgtaaacgtcgtccgaatctggcacgttggctggcatttgaagaacagccgtttgatgttattgttgcctatcgtgttgatcgtctgacccgtagcattcgtcatctgcaacagctggttcattgggcagaagatcataaaaaactggttgtgagcgcaaccgaagcacattttgataccaccaccccgtttgcagcagttgttattgcactgatgggcaccgttgcacagatggaactggaagcaattaaagaacgtaatcgtagcgcagcccattttaacattcgtgcaggtaaatatcgtggtagcctgcctccgtggggttatctgccgacgcgtgtagatggtgaatggcgtctggttcctgacccggttcagcgtgaacgtattctggaagtatatcatcgtgtggtggataatcatgaaccgctgcatctggttgcacatgatctgaatcgtcgtggtgttctgagtcccaaagattattttgctcagctgcaaggtcgtgaaccgcagggtcgtgaatggtctgcaaccgcactgaaacgtagcatgattagcgaagcaatgctgggttatgcaaccctgaatggtaaaaccgttcgtgatgatgatggtgcaccgctggttcgtgcagaaccgattctgacacgtgaacagctggaagcactgcgtgccgaactggttaaaaccagccgtgcaaaaccggcagttagcaccccgagcctgctgctgcgtgttctgttttgtgcagtttgtggtgaaccggcatacaaatttgccggtggtggtcgtaaacatccgcgttatcgttgtcgtagcatgggttttccgaaacattgtggtaatggtacagttgcaatggcagaatgggatgcattttgcgaagaacaggttctggatctgctgggtgatgccgaacgtctggaaaaagtttgggttgcaggtagcgatagcgcagttgaactggccgaagttaatgcggaactggtcgatctcaccagtctgattggaagtcccgcatatcgtgcgggtagtcctcagcgtgaagcactggatgcacgtattgcagcactggcagcacgtcaagaagaactggaaggtctggaagcacgtccgagcggttgggaatggcgtgaaacaggtcagcgttttggtgattggtggcgtgagcaggataccgcagcaaaaaatacctggctgcgtagtatgaatgttcgcctgacctttgatgttcgcggtggcctgacccgcaccattgattttggcgatctgcaagaatatgaacagcatctgcgtctgggtagcgttgttgaacgtctgcataccggcatgagcaccggcggtggcagcggcggttctggtggctctagcaaaggagaagaacttttcactggagttgtcccaattcttgttgaattagatggtgatgttaatgggcacaaattttctgtccgtggagagggtgaaggtgatgctacaaacggaaaactcacccttaaatttatttgcactactggaaaactacctgttccgtggccaacacttgtcactactctgacctatggtgttcaatgcttttcccgttatccggatcacatgaaacggcatgactttttcaagagtgccatgcccgaaggttatgtacaggaacgcactatatctttcaaagatgacgggacctacaagacgcgtgctgaagtcaagtttgaaggtgatacccttgttaatcgtatcgagttaaagggtattgattttaaagaagatggaaacattcttggacacaaactcgagtacaactttaactcacacaatgtatacatcacggcagacaaacaaaagaatggaatcaaagctaacttcaaaattcgccacaacgttgaagatggttccgttcaactagcagaccattatcaacaaaatactccaattggcgatggccctgtccttttaccagacaaccattacctgtcgacacaatctgtcctttcgaaagatcccaacgaaaagcgtgaccacatggtccttcttgagtttgtaactgctgctgggattacacatggcatggatgaactctacaaaaggcctgctgctaacgacgaaaactacgctctggctgcttaataagcggccgcggcctaggcggcctcctgtgtgaaattgttatccgctttaattaaaggcatcaaataaaacgaaaggctcagtcgaaagactgggcctttcgttttatctgttgtttgtcggtgaacgctctcctgagtaggacaaatccgccgccctagacagctgggcgcgcc")
-    frag_to_seq = extract_target_seq(test_seq)
+
+    frag_to_seq = extract_target_seq('test_csv.csv')
     print(frag_to_seq)
     print (len(frag_to_seq))
