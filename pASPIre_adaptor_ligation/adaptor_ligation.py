7e5b9244303135cb14d3119f2cbf5a6650608f70
Michael <160975477+MichaelSkehan@users.noreply.github.com>
2025-09-02 22:51:02 +0100

    testing out exporting csv

--- pASPIre_adaptor_ligation/adaptor_ligation.py
+++ pASPIre_adaptor_ligation/adaptor_ligation.py
@@ -1,6 +1,6 @@
 import adaptors
-from parse_csv import parse_csv
 from restriction_digest import extract_target_seq
+import csv
 
 def ligate(left, seq, right):
     """
@@ -19,14 +19,30 @@ def ligate(left, seq, right):
     """
     return left + seq + right
 
+
+
 if __name__ == '__main__':
-    list_of_ligated_sequences = []
-    for sample in extract_target_seq('test_csv.csv'):
-        sequence = sample[0]
-        left = adaptors.LEFT_ADAPTORS[sample[1]]
-        right = adaptors.RIGHT_ADAPTORS[sample[2]]
 
-        ligated_sequence = ligate(left, sequence, right)
-        list_of_ligated_sequences.append(ligated_sequence)
+    def main(csv_loc, export_csv=False, filename="ligated_sequences.csv"):
+        list_of_ligated_sequences = []
+        rows_for_csv = []
+        for sample in extract_target_seq(csv_loc):
+            sequence = sample[0]
+            left = adaptors.LEFT_ADAPTORS[sample[1]]
+            right = adaptors.RIGHT_ADAPTORS[sample[2]]
 
-    print(list_of_ligated_sequences)
+            ligated_sequence = ligate(left, sequence, right)
+            list_of_ligated_sequences.append(ligated_sequence)
+            rows_for_csv.append([ligated_sequence, sample[1], sample[2]])
+
+        if export_csv:
+            with open(filename, mode="w", newline="") as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(["Sequence", "Left_Adaptor", "Right_Adaptor"])  # header
+                writer.writerows(rows_for_csv)
+        print (rows_for_csv)
+
+
+        return list_of_ligated_sequences
+
+    print(main('test_csv.csv', export_csv=True, filename="ligated_sequences.csv"))
